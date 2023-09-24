e7c9ffe5524bad90df7db19e71ae839fff5b1249
Prakhar M <123287169+PM4305@users.noreply.github.com>
2023-09-24 17:01:49 +0530

    Update DDL.sql

--- DDL.sql
+++ DDL.sql
@@ -85,3 +85,38 @@ CREATE TABLE IF NOT EXISTS cruise_registration (
     country VARCHAR(100) NOT NULL,
     location VARCHAR(255) NOT NULL
 );
+CREATE TABLE IF NOT EXISTS hotel_details (
+    ID INT references hotel_registration(ID),
+    type varchar(50) NOT NULL,
+    total_number int NOT NULL,
+    available int NOT NULL,
+    fixed_price float(10,2) NOT NULL
+);
+CREATE TABLE IF NOT EXISTS flight_details (
+    ID INT references flight_registration(ID),
+    route varchar(100) NOT NULL,
+    total_number int NOT NULL,
+    type varchar(50) NOT NULL,
+    fixed_price float(10,2) NOT NULL
+);
+CREATE TABLE IF NOT EXISTS bus_details (
+    ID INT references bus_registration(ID),
+    route varchar(100) NOT NULL,
+    total_number int NOT NULL,
+    type varchar(50) NOT NULL,
+    fixed_price float(10,2) NOT NULL
+);
+CREATE TABLE IF NOT EXISTS cab_details (
+    ID INT references cab_registration(ID),
+    route varchar(100) NOT NULL,
+    total_number int NOT NULL,
+    type varchar(50) NOT NULL,
+    fixed_price float(10,2) NOT NULL
+);
+CREATE TABLE IF NOT EXISTS cruise_details (
+    ID INT references cruise_registration(ID),
+    route varchar(100) NOT NULL,
+    total_number int NOT NULL,
+    type varchar(50) NOT NULL,
+    fixed_price float(10,2) NOT NULL
+);
