02098aab4bc8bdfb22287fc3ab8a29ceb91eb3e9
ADARSH AMIT <130575709+adarsh1001111@users.noreply.github.com>
2023-09-24 13:06:09 +0530

    ddl_updated

--- DDL.sql
+++ DDL.sql
@@ -53,7 +53,7 @@ CREATE TABLE IF NOT EXISTS cab_registration (
     address VARCHAR(255) NOT NULL,
     pincode VARCHAR(10) NOT NULL,
     city VARCHAR(100) NOT NULL,
-    stat VARCHAR(100) NOT NULL,
+    state VARCHAR(100) NOT NULL,
     country VARCHAR(100) NOT NULL,
     location VARCHAR(255) NOT NULL
 );
@@ -67,7 +67,7 @@ CREATE TABLE IF NOT EXISTS bus_registration (
     address VARCHAR(255) NOT NULL,
     pincode VARCHAR(10) NOT NULL,
     city VARCHAR(100) NOT NULL,
-    stat VARCHAR(100) NOT NULL,
+    state VARCHAR(100) NOT NULL,
     country VARCHAR(100) NOT NULL,
     location VARCHAR(255) NOT NULL
 );
@@ -78,10 +78,10 @@ CREATE TABLE IF NOT EXISTS cruise_registration (
     email VARCHAR(255) NOT NULL,
     phone_number int NOT NULL,
     gst_number VARCHAR(20) NOT NULL,
-    address VARCHAR(255) NOT NULL,
+    HeadquaterAddress VARCHAR(255) NOT NULL,
     pincode VARCHAR(10) NOT NULL,
     city VARCHAR(100) NOT NULL,
-    stat VARCHAR(100) NOT NULL,
+    state VARCHAR(100) NOT NULL,
     country VARCHAR(100) NOT NULL,
     location VARCHAR(255) NOT NULL
 );
